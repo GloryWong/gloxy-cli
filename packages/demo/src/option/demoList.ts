e6256be5fbf0f08971bcadc333b95465c74b31a3
Glory Wong Zhaohui <glorywong1001@gmail.com>
2021-06-15 17:26:41 +0800

    chore: demo cli: comment ----

--- packages/demo/src/option/demoList.ts
+++ packages/demo/src/option/demoList.ts
@@ -3,31 +3,33 @@ import log from '@glorywong/log';
 import { prompt } from 'inquirer';
 import * as demo from './demo';
 
-function listDemos(): void {
+function listDemos(): boolean {
   try {
     const list = getDemoIndex();
     if (!list.length) {
       log.info('-- no demo --');
-      return;
+      return false;
     }
 
+    log.info('--------------------------------');
     list.forEach(({ code, name }: { code: number, name: string }) => {
       log.info('[', 'success:', code, 'info:', ']', name);
     });
+    log.info('--------------------------------');
+
+    return true;
   } catch (error) {
     log.error('list demos failed:', error);
+    return false;
   }
 }
 
 async function chooseDemo(): Promise<void> {
   try {
-    const list = getDemoIndex();
-    if (!list.length) {
-      log.info('-- no demo --');
+    if (!listDemos()) {
       return;
     }
 
-    listDemos();
     const { demoCode } = await prompt({
       type: 'input',
       name: 'demoCode',
