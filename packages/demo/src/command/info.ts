efb7dd0903e0d682bb0859c0f3a13f0f856795b0
Glory Wong Zhaohui <glorywong1001@gmail.com>
2021-06-25 04:15:36 +0800

    chore: demo cli: sexy font for GS Demo info title

--- packages/demo/src/command/info.ts
+++ packages/demo/src/command/info.ts
@@ -4,6 +4,8 @@ import log from '@glorywong/log';
 import { getInfo } from '../core/infoGSDemo';
 import boxen from 'boxen';
 import { cliVersion } from '../command-helper/cliInfo';
+import chalk from 'chalk';
+import cfonts from 'cfonts';
 
 new Command()
   .action(function() {
@@ -13,11 +15,22 @@ new Command()
       }
 
       const { location, name, description, demoCount } = getInfo();
-      const info = `
-        GSDemo CLI ${cliVersion}
+      const bigTitle = cfonts.render('GSDemo', {
+        font: 'block',
+        colors: ['system'],         // define all colors
+        background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+        letterSpacing: 1,           // define letter spacing
+        lineHeight: 1,              // define the line height
+        space: true,
+        gradient: ['red', 'blue'], 
+        env: 'node'
+      });
+
+      const info = `${bigTitle.string}
+        ${chalk.bold.green('GSDemo CLI')} ${chalk.bold.yellow(cliVersion)}
 
         Present GS Demo:
-          Name: ${name}
+          Name: ${chalk.bold(name)}
           Location: ${location}
           Description: ${description}
           Demo count: ${demoCount}
