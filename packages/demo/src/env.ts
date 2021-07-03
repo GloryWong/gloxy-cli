af5f857da51af03b42817a3276a4ff201aea616f
Glory Wong Zhaohui <glorywong1001@gmail.com>
2021-07-03 19:14:45 +0800

    fix: demo cli: npm files add .env

--- packages/demo/src/env.ts
+++ packages/demo/src/env.ts
@@ -2,4 +2,4 @@ import dotenvFlow from 'dotenv-flow';
 import path from 'path';
 
 const envPath = path.resolve(__dirname, '..');
-dotenvFlow.config({ path: envPath });
+dotenvFlow.config({ path: envPath, default_node_env: 'test' });
